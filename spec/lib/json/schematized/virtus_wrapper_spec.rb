b95d6de3090d87f5a4baf7e74b83a180843c8d70
Marcelo Manzan <manzan@gmail.com>
2013-01-24 13:03:09 -0200

    extracting shared examples for JSON::Schematized::Wrapper

--- spec/lib/json/schematized/virtus_wrapper_spec.rb
+++ spec/lib/json/schematized/virtus_wrapper_spec.rb
@@ -11,90 +11,10 @@ describe ::JSON::Schematized::VirtusWrapper do
   let(:schema_fixture_file){ File.expand_path("../../../../fixtures/person.yml", __FILE__) }
   let(:schema_str){ MultiJson.dump(YAML.load(File.read(schema_fixture_file))["person"]) }
 
+  # required helpers to shared examples
   let(:schema){ MultiJson.load(schema_str, :symbolize_keys => true) }
   let(:modularized_schema){ described_class.modularize(schema) }
   let(:model_class){ ::VPerson }
 
-  context "wrapper module" do
-    subject { described_class.modularize(schema) }
-    it { should be_kind_of Module }
-    it { should be_include JSON::Schematized::Models }
-    it { should be_include described_class::Models }
-    its(:name){ should =~ /\A#{described_class}::JSD/ }
-    its(:json_schema){ should == schema }
-  end
-
-  context "model classes" do
-    subject { model_class }
-    it { should be_include JSON::Schematized::Models }
-    it { should be_include described_class::Models }
-    it { should be_include described_class.modularize(schema) }
-    its(:json_schema){ should == schema }
-
-    context "attribute set names" do
-      subject { model_class.attribute_set.map(&:name).sort }
-      it { should == [:address, :children, :email, :phones] }
-    end
-
-    context "submodel types" do
-      it { should be_const_defined :Address }
-      it { should be_const_defined :ChildrenCollection }
-      it { should be_const_defined :Child }
-      it { should be_const_defined :PhonesCollection }
-    end
-  end
-
-  context "model instances" do
-    subject { model_class.new }
-    its(:address){ should be_kind_of model_class::Address }
-    its(:phones){ should be_kind_of model_class::PhonesCollection }
-    its(:children){ should_not be_instance_of ::Array }
-    its(:children){ should be_instance_of model_class::ChildrenCollection }
-    its(:children){ should be_kind_of described_class::Array }
-
-    context "with mass assignment" do
-      let(:phones){ ["555-1234"] }
-      let(:address){ {:street_name => "Wall Street", :number => 1000} }
-      let(:child){ {:name => "John", :age => 10} }
-      let :attrs do
-        {
-          :email => "me@email.com", :phones => phones,
-          :address => address, :children => [child]
-        }
-      end
-      subject { model_class.new attrs }
-      its(:email){ should == "me@email.com" }
-      its(:phones){ should == phones }
-      its(:address){ should be_instance_of model_class::Address }
-      its(:"address.street_name"){ should == address[:street_name] }
-      its(:"address.number"){ should == address[:number] }
-      its(:"children.size"){ should be 1 }
-      its(:"children.first"){ should be_instance_of model_class::Child }
-      its(:"children.first.name"){ should == child[:name] }
-      its(:"children.first.age"){ should == child[:age] }
-    end
-  end
-
-  context "collection classes" do
-    subject { model_class::ChildrenCollection }
-    it { should be_include JSON::Schematized::Collections }
-    it { should be_include described_class::Collections }
-  end
-
-  context "object" do
-    let(:object_model){ Object.new.extend(modularized_schema) }
-    subject { object_model }
-    before { object_model.children = [{}] }
-
-    it { should be_kind_of modularized_schema::ComplexTypes }
-    it { should be_respond_to :email }
-    it { should be_respond_to :address }
-    its(:address){ should be_kind_of modularized_schema::ComplexTypes::Address }
-    its(:phones){ should be_kind_of modularized_schema::ComplexTypes::PhonesCollection }
-    its(:children){ should_not be_instance_of ::Array }
-    its(:children){ should be_instance_of modularized_schema::ComplexTypes::ChildrenCollection }
-    its(:children){ should be_kind_of described_class::Array }
-    its(:"children.size"){ should be 1 }
-    its(:"children.first"){ should be_instance_of modularized_schema::ComplexTypes::Child }
-  end
+  it_should_behave_like "a JSON::Schematized::Wrapper"
 end
