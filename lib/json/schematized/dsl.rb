bd1f92ffebdca98dbd98b91f024e22abe6654e6a
Marcelo Manzan <manzan@gmail.com>
2013-01-28 10:56:50 -0200

    DSL: registering JSON-Schema loader block at @json_schema_loader

--- lib/json/schematized/dsl.rb
+++ lib/json/schematized/dsl.rb
@@ -6,8 +6,8 @@ module JSON
   module Schematized
     module DSL
       def json_schema(*args, &block)
-        if instance_variable_defined?(:"@json_schema")
-          schema = @json_schema[:loader].call
+        if instance_variable_defined?(:"@json_schema_loader")
+          schema = @json_schema_loader.call
           schema = MultiJson.dump(schema) unless schema.is_a?(String)
           MultiJson.load(schema, :symbolize_keys => true)
         else
@@ -16,7 +16,7 @@ module JSON
           json = args.first
           raise ArgumentError, "JSON or block expected" if block_given? ^ json.nil?
           block = Proc.new{ json } unless block_given?
-          @json_schema = {:loader => block}
+          @json_schema_loader = block
           wrapper = "#{opts.fetch(:wrapper, :none)}_wrapper".gsub(/(?:\A_*|_)([^_])/){ $1.upcase }.to_sym
           wrapper =  Schematized.const_defined?(wrapper) ? Schematized.const_get(wrapper) : nil
           send(:include, wrapper) if wrapper
