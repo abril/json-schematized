35ac9883b6a94190ad112f2891cad26f0ff1e3fb
Marcelo Manzan <manzan@gmail.com>
2013-01-31 01:51:08 -0200

    DSL: simplifying the inclusion of additional modules

--- lib/json/schematized/dsl.rb
+++ lib/json/schematized/dsl.rb
@@ -11,15 +11,13 @@ module JSON
           schema = MultiJson.dump(schema) unless schema.is_a?(String)
           MultiJson.load(schema, :symbolize_keys => true)
         else
-          return if self === Base
           opts = args.last.is_a?(Hash) ? args.pop : {}
           json = args.first
           raise ArgumentError, "JSON or block expected" if block_given? ^ json.nil?
           block = Proc.new{ json } unless block_given?
           @json_schema_loader = block
           wrapper = "#{opts.fetch(:wrapper, :basic)}_wrapper".gsub(/(?:\A_*|_)([^_])/){ $1.upcase }.to_sym
-          wrapper =  Schematized.const_defined?(wrapper) ? Schematized.const_get(wrapper) : nil
-          send(:include, wrapper) if wrapper
+          include Schematized.const_get(wrapper) if Schematized.const_defined?(wrapper)
           self
         end
       end
